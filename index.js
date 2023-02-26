7362bea2347a192ae7110a1607b7cb834336af88
Vali Manea <118940094+valimanea@users.noreply.github.com>
2023-02-26 20:24:23 +0000

    added questions and got resposes

---
+++ index.js
@@ -0,0 +1,140 @@
+// include modules
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  "What is your GitHub username?",
+  "What is your email address?",
+  "What is the title of your project?",
+  "Enter a short description of your project:",
+  "What type of License should your project have?",
+  "What command should be run to install dependencies?",
+  "What command should be run to run tests?",
+  "Enter the usage information of this repo:",
+  "Enter the contribution guidelines to this repo:",
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// function to initialize program
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "github",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a value!";
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "email",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a value!";
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[2],
+        name: "title",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a title!";
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "description",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a description!";
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        message: questions[4],
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license",
+      },
+      {
+        type: "input",
+        message: questions[5],
+        name: "install",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a command!";
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[6],
+        name: "test",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter a command!";
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[7],
+        name: "usage",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter some ussage info!";
+          }
+        },
+      },
+
+      {
+        type: "input",
+        message: questions[8],
+        name: "contributions",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return "Please enter some contribution info!";
+          }
+        },
+      },
+    ])
+    .then((response) => {
+      writeToFile("README.md", response)
+      console.log(response);
+    });
+}
+
+// function call to initialize program
+init();
